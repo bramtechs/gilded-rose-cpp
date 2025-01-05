84f2f618164ef03c111b05077e1c60fb70995031
bramtechs <bramtech@telenet.be>
2025-01-05 17:52:12 +0100

    run clang-format

--- src/GildedRose.h
+++ src/GildedRose.h
@@ -3,22 +3,23 @@
 
 using namespace std;
 
-class Item
-{
+class Item {
 public:
     string name;
     int sellIn;
     int quality;
-    Item(string name, int sellIn, int quality) : name(name), sellIn(sellIn), quality(quality) 
-    {}
+    Item(string name, int sellIn, int quality)
+        : name(name)
+        , sellIn(sellIn)
+        , quality(quality)
+    {
+    }
 };
 
-class GildedRose
-{
+class GildedRose {
 public:
-    vector<Item> & items;
-    GildedRose(vector<Item> & items);
-    
+    vector<Item>& items;
+    GildedRose(vector<Item>& items);
+
     void updateQuality();
 };
-
--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -4,19 +4,18 @@
 
 std::ostream& operator<<(std::ostream& os, const Item& obj)
 {
-	return os
-		<< "name: " << obj.name
-		<< ", sellIn: " << obj.sellIn
-		<< ", quality: " << obj.quality;
+    return os
+        << "name: " << obj.name
+        << ", sellIn: " << obj.sellIn
+        << ", quality: " << obj.quality;
 }
 
 // This is a normal unit test using Catch2
-TEST_CASE("UpdateQuality") {
-
-	vector<Item> items;
-	items.push_back(Item("foo", 0, 0));
-	GildedRose app(items);
-	app.updateQuality();
-	REQUIRE("fixme" == app.items[0].name);
+TEST_CASE("UpdateQuality")
+{
+    vector<Item> items;
+    items.push_back(Item("foo", 0, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    REQUIRE("fixme" == app.items[0].name);
 }
-
