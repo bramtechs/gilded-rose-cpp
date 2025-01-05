4cdbdbe99c78d46b5bd355d37c1b92247ca63591
bramtechs <bramtech@telenet.be>
2025-01-05 18:46:47 +0100

    clean up math operations

--- src/GildedRose.cc
+++ src/GildedRose.cc
@@ -20,37 +20,37 @@ void GildedRose::updateQuality()
     for (Item& item : items) {
         if (!isAgedBrie(item) && !isBackstagePass(item)) {
             if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
+                item.quality--;
             }
         } else if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality++;
 
             if (isBackstagePass(item)) {
                 if (item.sellIn < 11 && item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
                 }
 
                 if (item.sellIn < 6 && item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
                 }
             }
         }
 
         if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn = item.sellIn - 1;
+            item.sellIn--;
         }
 
         if (item.sellIn < 0) {
             if (!isAgedBrie(item)) {
                 if (!isBackstagePass(item)) {
                     if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1;
+                        item.quality--;
                     }
                 } else {
-                    item.quality = item.quality - item.quality;
+                    item.quality -= item.quality;
                 }
             } else if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                item.quality++;
             }
         }
     }
