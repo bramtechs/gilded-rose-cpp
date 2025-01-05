df307726a0731b8f4e27b2550846b9194c807a87
bramtechs <bramtech@telenet.be>
2025-01-05 18:03:08 +0100

    test cases for base rules (see requirements specification)

--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -10,14 +10,37 @@ std::ostream& operator<<(std::ostream& os, const Item& obj)
         << ", quality: " << obj.quality;
 }
 
-// This is a normal unit test using Catch2
-TEST_CASE("UpdateQuality")
+TEST_CASE("Check item name")
+{
+    std::vector<Item> items;
+    items.push_back(Item("foo", 0, 0));
+    GildedRose app(items);
+    REQUIRE("foo" == app.items[0].name);
+}
+
+TEST_CASE("UpdateQuality should decrease sellIn by one")
 {
     std::vector<Item> items;
     items.push_back(Item("foo", 0, 0));
     GildedRose app(items);
     app.updateQuality();
-    REQUIRE("foo" == app.items[0].name);
     REQUIRE(-1 == app.items[0].sellIn);
+}
+
+TEST_CASE("UpdateQualty should decrease quality by one")
+{
+    std::vector<Item> items;
+    items.push_back(Item("foo", 1, 1));
+    GildedRose app(items);
+    app.updateQuality();
     REQUIRE(0 == app.items[0].quality);
+}
+
+TEST_CASE("Negative sell by date, quality degrades twice as fast")
+{
+    std::vector<Item> items;
+    items.push_back(Item("foo", 0, 10));
+    GildedRose app(items);
+    app.updateQuality();
+    REQUIRE(8 == app.items[0].quality);
 }
